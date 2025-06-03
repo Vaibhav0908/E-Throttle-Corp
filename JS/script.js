252715ea84de615ed17864697d061e6c629e7a70
Vaibhav Samarth <vaibhavsamarth39@gmail.com>
2025-06-03 18:44:05 +0530

    Files Added

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+// var items = document.querySelectorAll('.carousel .carousel-item');
+// items.forEach((e) => {
+//     const slide = 4;
+//     let next = e.nextElementSibling;
+//     for (var i = 0; i < slide; i++) {
+//         if (!next) {
+//             next = items[0]
+//         }
+//         let clonechild = next.cloneNode(true)
+//         e.appendChild(clonechild.children[0])
+//         next = next.nextElementSibling
+//     }
+// })
+
+
+// $(document).ready(
+//     function () {
+//         $(".moon").click(function () {
+//             $("body").toggleClass("body_color_night")
+//             $(".moon").toggleClass("fas fa-sun")
+//             $("a").toggleClass("night_color")
+//             $(".dropdown-menu").toggleClass("body_color_night")
+//         }
+//         )
+//     }
+// )
